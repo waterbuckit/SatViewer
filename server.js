a8e8ab130df9883d1f762e6c830691d811f1ce39
waterbucket <adamroyal232@yahoo.com>
2019-02-02 12:32:39 +0000

    Added more server code to load CSV file to JSON objects

--- server.js
+++ server.js
@@ -1,5 +1,29 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var parse = require('csv-parse/lib/sync');
+var fs = require('fs');
+
+// Get input file
+var input;
+try{
+    input = fs.readFileSync('satdata.csv', 'utf8');
+}catch(e){
+    console.log("Could not find satdata.csv");
+    process.exit(1);
+}
+
+// JSON objects of satellite data 
+var records = parse(input, {
+    columns: true,
+    skip_empty_lines: true
+});
+
 var app = express();
 var server = app.listen(25565);
 app.use(express.static('views/public'));
+
+app.get("/allstars", function(req, res){
+    db.query("select * from stars", function(err, rows, fields){
+        res.send(rows);
+    });
+});
