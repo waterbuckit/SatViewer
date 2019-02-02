e77f8f629b7baaa6b2662784e52c80bee856b33f
unironically <lbvac230@gmail.com>
2019-02-02 12:51:01 +0000

    Added earth texture to sphere.

--- views/public/universe.js
+++ views/public/universe.js
@@ -1,9 +1,14 @@
+var d;
+
 function setup() {
-  createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  canvas = createCanvas(window.innerWidth, window.innerHeight, WEBGL);
+  img = loadImage('earth_day.jpg');
 }
 
 function draw() {
   background(0);
+  texture(img);
   sphere(100);
   orbitControl();
 }
+
